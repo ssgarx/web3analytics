0715c742ba9bf6c9cb9617294629c7b5b93bb814
ssgarx <sagarchandgadkar74@gmail.com>
2022-06-14 21:04:04 +0530

    test

--- index.js
+++ index.js
@@ -1,60 +1,63 @@
 import fetch from "node-fetch";
 const web3analytics = {
   init: (project_key) => {
-    fetch("http://localhost:3000/api/v1/view_event", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        project_key: project_key,
-        url: window.location.href,
-      }),
-    })
-      .then((res) => {
-        localStorage.setItem("web3analytics", res.data.user_id);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    console.log("init");
+    // fetch("http://localhost:3000/api/v1/view_event", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify({
+    //     project_key: project_key,
+    //     url: "",
+    //   }),
+    // })
+    //   .then((res) => {
+    //     localStorage.setItem("web3analytics", res.data.user_id);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
   },
   granular_event: (project_key, event_key, event_value) => {
-    fetch("http://localhost:3000/api/v1/granular_event", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        project_key: project_key,
-        url: window.location.href,
-        user_id: localStorage.getItem("web3analytics"),
-        event: {
-          type: event_key,
-          payload: event_value,
-        },
-      }),
-    })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    console.log("granular_event");
+    // fetch("http://localhost:3000/api/v1/granular_event", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify({
+    //     project_key: project_key,
+    //     url: window.location.href,
+    //     user_id: localStorage.getItem("web3analytics"),
+    //     event: {
+    //       type: event_key,
+    //       payload: event_value,
+    //     },
+    //   }),
+    // })
+    //   .then((res) => {
+    //     console.log(res);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
   },
   leave_event: (project_key) => {
-    if (localStorage.getItem("web3analytics")) {
-      fetch("http://localhost:3000/api/v1/leave_event", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          project_key: project_key,
-          url: window.location.href,
-          user_id: localStorage.getItem("web3analytics"),
-        }),
-      })
-        .then((res) => {
-          console.log(res);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
+    console.log("leave_event");
+    // if (localStorage.getItem("web3analytics")) {
+    //   fetch("http://localhost:3000/api/v1/leave_event", {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify({
+    //       project_key: project_key,
+    //       url: window.location.href,
+    //       user_id: localStorage.getItem("web3analytics"),
+    //     }),
+    //   })
+    //     .then((res) => {
+    //       console.log(res);
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //     });
+    // }
   },
 };
 export default web3analytics;
