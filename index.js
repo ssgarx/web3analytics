0132a5b2866e6d458c4e34d406ad9bcc00c47eae
ssgarx <sagarchandgadkar74@gmail.com>
2022-06-14 11:37:53 +0530

    added server

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const isStringx = (input) => {
-  return typeof input === "string" || input instanceof String;
+import fetch from "node-fetch";
+const function1 = (event) => {
+  console.log("function1");
+  fetch("http://localhost:3000/event", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ event: event }),
+  }).then((res) => {
+    console.log("Request complete! response:", res);
+  });
 };
 
-module.exports = isStringx;
+module.exports = {
+  function1,
+};
